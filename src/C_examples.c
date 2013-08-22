61e34fd4bf972a041bc49e63b8e498bf45435fd9
dragon <dragon@dragon-Lenovo.(none)>
2013-08-22 14:36:05 +0800

    Hello World

---
+++ src/C_examples.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : C_examples.c
+ Author      : 叶玉龙
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
